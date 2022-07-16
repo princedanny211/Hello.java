c2cf440a032b71b0be486d9862e3c168902a3df0
HtechCloud <htechglobalinvest@gmail.com>
2022-07-16 16:08:47 -0400

    Java file added

---
+++ javac/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+   public static void main (string[] args) {
+      // prints "Hello and welcome to today session" in the terminal window
+      system.out.println("Hello and welcome to today session"); 
+   }
+}
